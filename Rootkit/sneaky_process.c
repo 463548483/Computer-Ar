cba87f7b39474e3958b32ef38e468987afbacb07
zw255 <zw255@duke.edu>
2022-04-02 11:19:11 -0400

    init Rootkit

---
+++ Rootkit/sneaky_process.c
@@ -0,0 +1,15 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+
+static int lkm_init(void){
+    printk("Arciryas:moduleloaded\n");
+    return 0;
+}
+
+static void lkm_exit(void){
+    printk("Arciryas:moduleremoved\n");
+}
+
+module_init(lkm_init);
+moduke_exit(lkm_exit);
